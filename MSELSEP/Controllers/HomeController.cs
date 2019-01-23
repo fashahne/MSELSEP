4727e3ca9ea4be8bc32979d6a090080f3ad51bd4
Farhan Shahnewaz <fashahne@microsoft.com>
2019-01-22 20:36:45 -0500

    Added registation

--- MSELSEP/Controllers/HomeController.cs
+++ MSELSEP/Controllers/HomeController.cs
@@ -55,6 +55,10 @@ namespace MSELSEP.Controllers
         {
             return View();
         }
+        public IActionResult Course()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {

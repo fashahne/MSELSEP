179b55eb1fa324f2074b695bddd95d87ff5ae058
Farhan Shahnewaz <fashahne@microsoft.com>
2021-03-10 04:34:37 -0500

    tst4

--- MSELSEP/Controllers/CSCColController.cs
+++ MSELSEP/Controllers/CSCColController.cs
@@ -10,7 +10,7 @@ namespace MSELSEP.Controllers
     {
         static async void MakeRequest()
         {
-            //perform good
+            //perform good ok 
             var client = new HttpClient();
             var queryString = HttpUtility.ParseQueryString(string.Empty);
 

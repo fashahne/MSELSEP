69d7eb567a58895749193c90f6b703b8d27a1710
Farhan Shahnewaz <fashahne@microsoft.com>
2021-03-18 17:46:03 -0400

    added decision tree class

---
+++ MSELSEP/Controllers/DecisionController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSELSEP.Controllers
+{
+    public class DecisionController
+    {
+        //replicating decision tree for automation
+    }
+}
